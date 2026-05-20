b81ca9ea81d58a562ba86830544845fcb94838ab
Vitalii Lazebnyi <18521734+VitaliiLazebnyi@users.noreply.github.com>
2026-05-21 08:19:58 +0900

    enforce code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,7 @@ end
 # Coverage mandates (SimpleCov)
 SimpleCov.start do
   enable_coverage :branch
+  minimum_coverage line: 100, branch: 100
   add_filter '/spec/'
   add_filter '/config/'
   formatter SimpleCov::Formatter::AIFormatter
